7f583989c6327afddc594fcbbaa2602cc872c75f
Bruno Cabral <bruno.cabral88@hotmail.com>
2016-03-23 17:02:08 -0300

    Initial commit
    Initial commit with the script and Readme.

---
+++ Hyper-Vmon.ps1
@@ -0,0 +1,34 @@
+﻿#Hyper-V Replicas report script
+#This script requires Powershell 3.0
+
+#Servers to check
+$hosts = @("Server01","Server02","Server03","Server04")
+
+#SMTP Server (Unauthenticated)
+$smtpServer = smtp.provider.com
+
+#From
+$from = "From@your-email.com" 
+
+#Recipient for the report
+[string[]]$recipients = "your@email.com","another@email.com"
+
+$message_body = @("<html><body>","Hyper-V Replica daily report","")
+
+#Retrieves information from servers in $hosts variable
+foreach ($element in $hosts) { $message_body += "=============================";
+    $message_body += "Status for $element VMs";
+    $message_body += "=============================";
+    $message_body += Invoke-Command -ComputerName $element -Scriptblock { Get-VMReplication | format-table | Out-String | ForEach-Object { $_ -replace "Warning","<font color='FF0000'>Warning</font>" } } 
+    
+    }
+
+$message_body += "</body></html>"
+
+#Formats the $message_body content
+$message_body2 = $message_body -replace "(?m)$","<br/>"
+
+$body = [string]::Join([Environment]::NewLine,$message_body2);
+
+#Sends out the e-mail
+Send-MailMessage -From $from -Subject "Hyper-V VMs Replication Reports" -To $recipients -SmtpServer $smtpServer -BodyAsHtml $body
---
+++ README.md
@@ -0,0 +1,6 @@
+# Hyper-V Replica Monitor
+
+Using Powershell, this script may be useful for those who needs to receive status reports for the Hyper-V replica.
+
+Powershell 3.0 required.
+To use it, you may simply run it or create a new scheduled task on Windows with the frequency you find suits the best for yours needs.
